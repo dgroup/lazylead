3ca2aa317160c202831467f983f866aa48c97b85
Yurii Dubinka <yurii.dubinka@gmail.com>
2020-03-04 00:20:16 +0200

    #43: Email - way how to setup email integration

---
+++ test/lazylead/email_test.rb
@@ -0,0 +1,49 @@
+# The MIT License
+#
+# Copyright (c) 2019-2020 Yurii Dubinka
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom
+# the Software is  furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+require_relative "../test"
+require_relative "../../lib/lazylead/mail"
+require_relative "../../lib/lazylead/allocated"
+
+module Lazylead
+  class EmailTest < Lazylead::Test
+
+    # @todo #/DEV email-related properties should be exported to the CI env
+    test "test email has been sent to the remote server" do
+      Lazylead::Mail.new.enable_notifications(
+        {
+          smtp_host: ENV["LL_SMTP_HOST"],
+          smtp_port: ENV["LL_SMTP_PORT"],
+          smtp_user: ENV["LL_SMTP_USER"],
+          smtp_pass: ENV["LL_SMTP_PASS"]
+        }
+      )
+      Mail.deliver do
+        from    'jlazylead@yandex.ru'
+        to      'ydantezs@yandex.ru'
+        subject 'Testing'
+        body 'Good, it works'
+      end
+    end
+  end
+end
+
