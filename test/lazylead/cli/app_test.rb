e7ab7b48da7560b6b1cc0b3cf1ff743149b711a6
Yurii Dubinka <yurii.dubinka@gmail.com>
2020-05-05 02:38:23 +0300

    #83: Fake schedule object for unit testing purposes

--- test/lazylead/cli/app_test.rb
+++ test/lazylead/cli/app_test.rb
@@ -47,7 +47,7 @@ module Lazylead
       end
 
       test "activesupport is activated for access to domain entities" do
-        CLI::App.new(Log::NOTHING, Schedule.new(cling: false)).run(
+        CLI::App.new(Log::NOTHING, Schedule.new(Log::NOTHING, false)).run(
           home: ".",
           sqlite: "test/resources/#{no_ext(__FILE__)}.#{__method__}.db",
           vcs4sql: "upgrades/sqlite",
