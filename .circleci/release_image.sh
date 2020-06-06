421a75c9a0dbe1cf7dcfb3987de643a9920484fe
Yurii Dubinka <yurii.dubinka@gmail.com>
2020-06-06 19:13:06 +0300

    #143: Upgrade to v0.1.1

--- .circleci/release_image.sh
+++ .circleci/release_image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export docker_release_tags="latest 1 1.0.1 0.1.1"
-docker login --username $DOCKER_USER --password $DOCKER_TOKEN
+export docker_release_tags="latest 1 1.0 0.1.1"
+docker login --username "${DOCKER_USER}" --password "${DOCKER_TOKEN}"
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
     for tag in ${docker_release_tags// / } ; do
-        docker tag dgroup/lazylead:$CIRCLE_BRANCH dgroup/lazylead:${tag}
-        docker push dgroup/lazylead:${tag}
+        docker tag "dgroup/lazylead:${CIRCLE_BRANCH}" "dgroup/lazylead:${tag}"
+        docker push "dgroup/lazylead:${tag}"
         echo "dgroup/lazylead:${tag} released"
     done
 else
-    docker push dgroup/lazylead:$CIRCLE_BRANCH
+    docker push "dgroup/lazylead:${CIRCLE_BRANCH}"
     echo "dgroup/lazylead:${CIRCLE_BRANCH} released"
 fi
