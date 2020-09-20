e97aed46139dea670ed39a33d1829606dc02739c
Yurii Dubinka <yurii.dubinka@gmail.com>
2020-09-20 21:05:03 +0300

    #219: ensure that jira ticket has a log file more than 10KB in attachments, LL accuracy.rb rule extension Fixed #219

---
+++ lib/lazylead/task/accuracy/logs.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# The MIT License
+#
+# Copyright (c) 2019-2020 Yurii Dubinka
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom
+# the Software is  furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+require_relative "attachment"
+
+module Lazylead
+  # Check that ticket has log file(s) in attachment.
+  class Logs < Lazylead::Attachment
+    def initialize
+      super("Log files", 2, "Attachments")
+    end
+
+    # Ensure that ticket has a '*.log' file more '10KB'
+    def matching(attach)
+      attach.attrs["size"].to_i > 10_240 &&
+        attach.attrs["filename"].match?(/(\s|\S)*.log\d*/)
+    end
+  end
+end
