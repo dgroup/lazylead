98c8b989cc69796583f574797381e625d1c20430
Yurii Dubinka <yurii.dubinka@gmail.com>
2020-10-16 20:17:04 +0300

    #241: Labels are not set to accuracy

--- lib/lazylead/system/jira.rb
+++ lib/lazylead/system/jira.rb
@@ -236,8 +236,8 @@ module Lazylead
     end
 
     def add_label(label, *more)
-      return if labels.nil?
       lbl = labels
+      lbl = [] if lbl.nil?
       lbl << label
       lbl += more if more.size.positive?
       save!("fields" => { "labels" => lbl.uniq })
