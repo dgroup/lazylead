076a7dfbe9a20372ef87630d4f8e54ba1dbc43bf
Yurii Dubinka <yurii.dubinka@gmail.com>
2021-03-09 22:48:53 +0200

    #328: Upgrade copyright to 2021 Fixed #328

--- test/lazylead/cli/app_test.rb
+++ test/lazylead/cli/app_test.rb
@@ -2,10 +2,10 @@
 
 # The MIT License
 #
-# Copyright (c) 2019-2020 Yurii Dubinka
+# Copyright (c) 2019-2021 Yurii Dubinka
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"],
+# of this software and associated documentation files (the "Software"),
 # to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense,
 # and/or sell copies of the Software, and to permit persons to whom
